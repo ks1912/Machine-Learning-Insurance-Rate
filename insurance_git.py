1f937f69a09f38dcb0d983c0389ffbd1c2fd096a
Kunal Singh <46413519+ks1912@users.noreply.github.com>
2020-02-24 00:46:00 +0530

    Add files via upload

---
+++ insurance_git.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+#CREATING DATA SET'S
+dataset = pd.read_csv('insurance.csv')
+Y = dataset.iloc[:, 6].values
+X = dataset.iloc[:, 0:1].values                       
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+# Predicting the Test set results
+Y_pred = regressor.predict(X_test)
+
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
